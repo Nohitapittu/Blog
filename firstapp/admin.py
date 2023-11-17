498803d8ba48f01cbdde40b09f997b8c9f88b692
Nohitapittu <nohitapittu87@gmail.com>
2023-11-17 10:09:43 +0530

    first commit

---
+++ firstapp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from firstapp.models import Post,Comment
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+
+
+
+
+
+# Register your models here.
+
+
+# Register your models here.
